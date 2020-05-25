77d48d9021a713cab52f99188ea7b6c6632da480
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-05-25 18:15:45 -0400

    connecting the app, preparing the first views and setting the context files

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import injectContext from './store/appContext';
+import Home from './views/home';
+import NotFound from './views/notfound';
 
-function App() {
+const App = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default injectContext(App)
---
+++ front/src/views/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+
+function Home() {
+  return (
+    <>
+      <h1>Aqui estamos!!!</h1>
+    </>
+  );
+}
+
+export default Home;
