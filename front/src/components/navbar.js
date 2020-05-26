1ddc23b207c63a2b38809d16457a4e000f7ae3da
Jerónimo Santa María Illanes <jerosantamariai@gmail.com>
2020-05-26 08:03:16 -0400

    fixed some styles stuffs

--- front/src/components/navbar.js
+++ front/src/components/navbar.js
@@ -1,38 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../images/logo-img.png';
 
 function Navbar() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark col-md-3" id="leftnav">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark col-md-3 d-lg-flex align-items-center" id="leftnav">
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                    <ul className="nav flex-column">
+                <div className="collapse navbar-collapse d-flex justify-content-center">
+                    <ul className="nav flex-column vertnav">
                         <li className="nav-item">
-                            <Link className="nav-link active" to="/">Home</Link>
+                            <Link to="/"><img src={logo} alt="logo" /></Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/aboutus">About Us</Link>
+                        <button type="button" className="btn btn-dark btn-block btn-sm"><Link className="nav-link noDecoration active" to="/">Home</Link></button>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="ourservices">Our Sevices</Link>
+                        <button type="button" className="btn btn-dark btn-block btn-sm"><Link className="nav-link noDecoration" to="/aboutus">About Us</Link></button>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/ourbarbers">Our Barbers</Link>
+                        <button type="button" className="btn btn-dark btn-block btn-sm"><Link className="nav-link noDecoration" to="ourservices">Our Sevices</Link></button>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/appointment">Appointment</Link>
+                        <button type="button" className="btn btn-dark btn-block btn-sm"><Link className="nav-link noDecoration" to="/ourbarbers">Our Barbers</Link></button>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/contact">Contact</Link>
+                        <button type="button" className="btn btn-dark btn-block btn-sm"><Link className="nav-link noDecoration" to="/appointment">Appointment</Link></button>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/login">Log In</Link>
+                        <button type="button" className="btn btn-dark btn-block btn-sm"><Link className="nav-link noDecoration" to="/contact">Contact</Link></button>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/register">Register</Link>
+                        <button type="button" className="btn btn-dark btn-block btn-sm"><Link className="nav-link noDecoration" to="/login">Log In</Link></button>
+                        </li>
+                        <li className="nav-item">
+                        <button type="button" className="btn btn-dark btn-block btn-sm"><Link className="nav-link noDecoration" to="/register">Register</Link></button>
                         </li>
                     </ul>
                 </div>
